ca9247118871ef27402a4947ec0be60cdf2876b4
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2020-01-14 21:51:48 +0000

    refactor

--- src/intcodeComputer.js
+++ src/intcodeComputer.js
@@ -10,36 +10,33 @@ function createArrayFromFile(file) {
   return processedData;
 }
 
+function processValues(intArray, i) {
+  const firstElemPosition = intArray[i + 1];
+  const secondElemPosition = intArray[i + 2];
+  const resultPosition = intArray[i + 3];
+
+  const sum =
+    intArray[i] === 1
+      ? intArray[firstElemPosition] + intArray[secondElemPosition]
+      : intArray[firstElemPosition] * intArray[secondElemPosition];
+
+  return { resultPosition, sum };
+}
+
 module.exports = {
   runProgramme: function(file) {
     const intArray = createArrayFromFile(file);
 
     for (let i = 0; i < intArray.length + 1; i++) {
-      if (intArray[i] === 1) {
-        firstElemPosition = intArray[i + 1];
-        secondElemPosition = intArray[i + 2];
-        resultPosition = intArray[i + 3];
-
-        const sum = intArray[firstElemPosition] + intArray[secondElemPosition];
+      if (intArray[i] === 1 || intArray[i] === 2) {
+        const { resultPosition, sum } = processValues(intArray, i);
         intArray[resultPosition] = sum;
-        // step forward 4 positions, beause the for loop
-        // also increments i, we only add 3 to it
-        i = i + 3;
-      } else if (intArray[i] === 2) {
-        firstElemPosition = intArray[i + 1];
-        secondElemPosition = intArray[i + 2];
-        resultPosition = intArray[i + 3];
-
-        const sum = intArray[firstElemPosition] * intArray[secondElemPosition];
-        intArray[resultPosition] = sum;
-        // step forward 4 positions, beause the for loop
-        // also increments i, we only add 3 to it
+        // step forward 4 positions, the loop also increments i, so only add 3
         i = i + 3;
       } else if (intArray[i] === 99) {
         break;
       }
     }
-
     return intArray;
   }
 };
--- test/intcodeComputer.js
+++ test/intcodeComputer.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 
 const intcodeComputer = require("../src/intcodeComputer");
 
-describe("Intcode Computer", () => {
+describe.only("Intcode Computer", () => {
   it("processes input 1 (addition) correctly", () => {
     const expectedOutput = [2, 0, 0, 0, 99];
     const actualOutput = intcodeComputer.runProgramme(
@@ -19,7 +19,7 @@ describe("Intcode Computer", () => {
     assert.deepStrictEqual(actualOutput, expectedOutput);
   });
 
-  it.only("processes input 2 (multiplication) correctly", () => {
+  it("processes input 2 (multiplication) correctly", () => {
     const expectedOutput = [2, 3, 0, 6, 99];
     const actualOutput = intcodeComputer.runProgramme(
       "test/fixtures/intcodeListInput2.txt"
